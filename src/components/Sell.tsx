1a3f9e1f5ccdf42927d78cc1fcd8af6c6b2bef66
Blajan George <george.blajan@sentiocx.com>
2023-03-29 15:01:51 +0300

    sell page

--- src/components/Sell.tsx
+++ src/components/Sell.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import style from "./css/sell.module.css"
-import { Button } from "@mui/material";
+import { Autocomplete, Button, TextField } from "@mui/material";
 
 function Sell() {
     return (
@@ -19,18 +19,22 @@ function Sell() {
         <label>
             <input type="text" placeholder="Price" className={style.field_4}/>
         </label>
-        <label>
-            <input type="text" placeholder="!Valute!" className={style.field_5}/>
-        </label>
+        <Autocomplete
+          value="€ Euro"
+          options={["€ Euro", "$ Dollar", "£ Pound"]}
+          sx={{ width: '15%', position:'absolute',marginLeft:'50%', marginTop:'40%'}}
+          renderInput={(params) => <TextField {...params}/>}/>
         <label>
             <input type="text" placeholder="Ms" className={style.field_6}/>
         </label>
         <label>
             <input type="text" placeholder="Rooms" className={style.field_8}/>
         </label>
-        <label>
-            <input type="text" placeholder="!Type!" className={style.field_7}/>
-        </label>
+        <Autocomplete
+          value="Apartment"
+          options={["Apartment", "House"]}
+          sx={{ width: '15%', position:'absolute',marginLeft:'50%', marginTop:'45%'}}
+          renderInput={(params) => <TextField {...params}/>}/>
         <img className={style.logo_2} src="src/assets/sell_logo_1.svg" width="100px"/>
         <Button className={style.button} variant="contained" sx={{background:"linear-gradient(#6EEE40, #52C9A6)"}}>PUBLISH</Button>
         </>
